ae29e964188dae54f283c5ef41a13d946ca95077
Mischa <s1118480@student.windesheim.nl>
2018-05-03 12:36:11 +0200

    Directory toegevoegd

---
+++ Views/layout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mischa-PC
+ * Date: 03/05/2018
+ * Time: 12:26
+ */
---
+++ htdocs/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mischa-PC
+ * Date: 03/05/2018
+ * Time: 12:19
+ */
+
+session_start();
+
+// the location where the Infinity files are stored
+define('APP_PATH', 'C:/xampp/infinity');
+
+// create a db connection
+require_once(APP_PATH .'/Model/db-connection.php');
+
+// show layout of Infinity
+require_once(APP_PATH .'Views/layout.php');

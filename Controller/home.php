f0f788dacc93806174539cd1606cc736642044db
Mischa <s1118480@student.windesheim.nl>
2018-05-03 12:38:18 +0200

    controller toegevoegd

--- Controller/home.php
+++ Controller/home.php
@@ -5,3 +5,18 @@
  * Date: 03/05/2018
  * Time: 12:01
  */
+include (APP_PATH. 'Model/Classes/User.php');
+//login page
+function loginform() {
+    require_once APP_PATH . '/Views/login.php';
+}
+
+function loginhandler() {
+    $hash = getPasswordHash($_POST['email']);
+
+    if (password_verify($_POST['password'], $hash)) {
+        $_SESSION['admin'] = true;
+        header("Location: /infinity/index.php?controller=rooster&action=showall");
+        exit;
+    }
+}

46e6c783ac3dffd3006e85bc05a8f48c5e466a67
Mischa <s1118480@student.windesheim.nl>
2018-05-03 13:21:45 +0200

    Controller toegevoegd

--- Controller/home.php
+++ Controller/home.php
@@ -5,7 +5,7 @@
  * Date: 03/05/2018
  * Time: 12:01
  */
-include (APP_PATH. 'Model/Classes/User.php');
+include (APP_PATH. '/Model/Classes/User.php');
 //login page
 function loginform() {
     require_once APP_PATH . '/Views/login.php';

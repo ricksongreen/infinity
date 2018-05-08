c37a06445038bb9ecffb3066237bc01e6edb3593
Sten <s1118010@student.windesheim.nl>
2018-05-08 12:54:29 +0200

    message

--- Model/admin.php
+++ Model/admin.php
@@ -5,3 +5,10 @@
  * Date: 8-5-2018
  * Time: 12:41
  */
+function addUser($user) {
+    global $dbh;
+
+    $stmt = $dbh->prepare("INSERT INTO user (first_name, last_name, password, country, email, Admin) VALUES(:first_name, :last_name, :password, :country, :email, :Admin)");
+
+    return $stmt->execute($customer);
+}

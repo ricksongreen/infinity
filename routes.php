4bd8374d7f0be5d3ac055f00551567027dd15f07
Sten <s1118010@student.windesheim.nl>
2018-05-09 08:08:50 +0200

    routes aangepast

--- routes.php
+++ routes.php
@@ -30,7 +30,10 @@ if (isset($_GET['controller']) && isset($_GET{'action'})) {
 
 // a list of the controllers we have and their actions we consider "allowed" values
 $allowedControllers = array(
-    'home' => array ('loginform', 'loginhandler'),
+    'home' => array ('loginform', 'loginhandler', 'addForm'),
+);
+$allowedControllers['admin/admin'] = array (
+    'addForm', 'add'
 );
 
 // allow extra controller when user is logged in as admin(systemowner)

84bb0609e848fdae4ffb34f529cda59d8048849b
Rick <s1118875@student.windesheim.nl>
2018-05-09 18:22:37 +0200

    fixed het bijvoegen van gebruikers

--- Model/admin.php
+++ Model/admin.php
@@ -7,28 +7,64 @@
  */
 function addUser($user) {
     global $dbh;
+    /** checks if the username is already stored in the database, so the usernames stay unique */
     try{
         $stmt = $dbh->prepare("SELECT gebruikersnaam FROM gebruiker WHERE gebruikersnaam=:gebruikersnaam");
         $values = array(
-            $gebruikersnaam = $user['gebruikersnaam'];
-        )
-        $doublecheck = $stmt->execute($values);
-        if(!empty($doublecheck)){
+            'gebruikersnaam' => $user['gebruikersnaam']
+        );
+        $stmt->execute($values);
+        $doublecheck = $stmt->fetch();
+        if(!empty($doublecheck['gebruikersnaam'])){
             throw new Exception("Gebruikersnaam is al in gebruik");
         }
     }catch(Exception $e){
         echo "ERROR: ".$e->getMessage();
         exit;
     }
+    /** Sets the user in the database */
     $stmt = $dbh->prepare("INSERT INTO gebruiker (voornaam, tussenvoegsel, achternaam, email, gebruikersnaam, wachtwoord) VALUES(:voornaam, :tussenvoegsel, :achternaam, :email, :gebruikersnaam, :wachtwoord)");
-
-    $stmt->execute($user);
+    $storeUser = array(
+        'voornaam' => $user['voornaam'],
+        'tussenvoegsel' => $user['tussenvoegsel'],
+        'achternaam' => $user['achternaam'],
+        'email' => $user['email'],
+        'gebruikersnaam' => $user['gebruikersnaam'],
+        'wachtwoord' => $user['wachtwoord']
+    );
+    $stmt->execute($storeUser);
 
     $stmt = $dbh->prepare("SELECT ID FROM gebruiker WHERE gebruikersnaam=:gebruikersnaam");
     $stmt->execute($values);
     $ID = $stmt->fetch();
 
-    //if
-
-    //$stmt = $dbh->prepare("")
+    if($user['student']==true){
+        $stmt = $dbh->prepare("INSERT INTO student(ID, nummer, opleiding) VALUES (:ID, :nummer, :opleiding)");
+        $values = array(
+            'ID' => $ID['ID'],
+            'nummer' => $user['nummer'],
+            'opleiding' => $user['opleiding']
+        );
+        $stmt->execute($values);
+    }else{
+        /** Sets the data for the SLB and admin, since the database does not correctly use true/false */
+        if($user['SLB'] == true) {
+            $SLB = 1;
+        }else{
+            $SLB = 0;
+        }
+        if($user['admin'] == true){
+            $admin = 1;
+        }else{
+            $admin = 0;
+        }
+        $stmt = $dbh->prepare("INSERT INTO werknemer(ID, nummer, studentbegeleider, systeembeheerder) VALUES(:ID, :nummer, :studentbegeleider, :systeembeheerder)");
+        $values = array(
+            'ID' => $ID['ID'],
+            'nummer' => $user['nummer'],
+            'studentbegeleider' => $SLB,
+            'systeembeheerder' => $admin
+        );
+        $stmt->execute($values);
+    }
 }

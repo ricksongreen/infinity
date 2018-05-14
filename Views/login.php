bc1de9035b97acd1c3d1e680aabc33d77bccba47
Bavlly <bavlly2008@hotmail.com>
2018-05-14 19:48:53 +0200

    transparant maken van loginbox

--- Views/login.php
+++ Views/login.php
@@ -18,7 +18,7 @@
     <img src="resources/avatar1.jpg" class="avatar">
     <h1>Login</h1>
     <form method="post" action="index.php?controller=home&action=loginhandler">
-        <p>Username</p><br>
+        <p>Username</p>
         <input type="text" name="username" placeholder="Enter Username" id="key" required><br><br>
         <p>Password</p>
         <input type="password" name="password" placeholder="Enter Password" required>

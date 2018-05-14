5d74b8a0e260f8cddfed4d207ff9ac0d83dcbd58
Bavlly <bavlly2008@hotmail.com>
2018-05-14 19:31:59 +0200

    layout en login aangepast maar niks verwijders

--- Views/layout.php
+++ Views/layout.php
@@ -2,21 +2,60 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Infinity</title>
-</head>
+    <style> ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: transparent;
+        }
 
-<nav><hr>
-<b> Menu:</b>
-<button>Home</button>
-    <button><a href ='index.php'>Login</a></button>
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: #4CAF50;
+        }
+    </style>
+</head>
+<body>
+<!--<nav>-->
+<!--<b> Menu:</b>-->
+<!--    <button><a href ='index.php'>Login</a></button>-->
+<!--    --><?php
+//    if($_SESSION["rechten"] == 'admin' or $_SESSION["rechten"] == 'slbadmin'){
+//    echo "<a href='index.php?controller=admin/admin&action=addForm'>Admin</a>";
+//    echo "<a href='index.php?controller=admin/admin&action=showUsers'>Student List</a>";
+//    }?>
+<!--</nav>-->
+
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="index.php">Login</a></li>
     <?php
     if($_SESSION["rechten"] == 'admin' or $_SESSION["rechten"] == 'slbadmin'){
-    echo "<a href='index.php?controller=admin/admin&action=addForm'>Admin</a>";
-    echo "<a href='index.php?controller=admin/admin&action=showUsers'>Student List</a>";
+        echo "<a href='index.php?controller=admin/admin&action=addForm'>Admin</a>";
+        echo "<a href='index.php?controller=admin/admin&action=showUsers'>Student List</a>";
     }?>
-<hr></nav>
-<br />
+    <li style="float:right"><a  href="index.php">T08</a></li>
+</ul>
+
+
 <?php require(APP_PATH . '/routes.php'); ?>
 
 <!--<footer>-->
@@ -24,4 +63,6 @@
 <!--    <hr>-->
 <!--    &copy; 2018 - Infinity Team 8-->
 <!--</footer>-->
+
+</body>
 </html>
--- Views/login.php
+++ Views/login.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body>
 <div class="loginbox">
@@ -24,6 +25,7 @@
         <input type="submit">
     </form>
 </div>
+
 
 
 </body>

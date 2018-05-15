ff500561064df3ac3179db33b0a16fce2c2aa4ff
Bavlly <bavlly2008@hotmail.com>
2018-05-15 11:47:08 +0200

    rest

--- Views/admin/allUsers.php
+++ Views/admin/allUsers.php
@@ -11,7 +11,7 @@
 <head>
     <title>Infinity</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="htdocs/style.css">
 </head>
 
 

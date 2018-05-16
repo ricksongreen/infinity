9269880b5c05bea3586e9a2e345cb5691a160ecc
ricksongreen <ricksongreen@hotmail.com>
2018-05-16 23:35:21 +0200

    changed the checkboxes so that the text can also be clicked

--- Views/admin/registration.php
+++ Views/admin/registration.php
@@ -14,7 +14,7 @@
 <form method="post" action="index.php?controller=admin/admin&action=add">
     <div class="container">
         <h1>Infinity Registratie</h1>
-        <label for="email"><b>Voornaam</b> <input type="text" name="voornaam" required><br>
+            <b>Voornaam</b> <input type="text" name="voornaam" required><br>
             <b>Tussenvoegsel</b> <input type="text" name="tussenvoegsel"><br>
             <b>Achternaam</b> <input type="text" name="achternaam" required><br>
             <b>Email</b> <input type="email" name="email" required><br>
@@ -22,10 +22,10 @@
             <b>Wachtwoord</b> <input type="password" name="wachtwoord" required><br>
             <b>Student-/Werknemersnummer</b> <input type="text" name="nummer" ><br>
             <b>Opleiding Student</b> <input type="text" name="opleiding"><br>
-            <input type="checkbox" name="student"> Student<br>
-            <input type="checkbox" name="docent"> Docent<br>
-            <input type="checkbox" name="SLB"> Studieloopbaan begeleider<br>
-            <input type="checkbox" name="admin"> Admin<br>
+            <input type="checkbox" id="student" name="student"><label for="student"> Student</label><br>
+            <input type="checkbox" id="docent" name="docent"><label for="docent"> Docent</label><br>
+            <input type="checkbox" id="SLB" name="SLB"><label for ="SLB"> Studieloopbaan begeleider</label><br>
+            <input type="checkbox" id="admin" name="admin"><label for="admin"> Admin</label><br>
             <input type="submit" value="Verstuur" class="registerbtn">
     </div>
 </form>

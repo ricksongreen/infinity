f990e7b656b8f7efd0b386f1a2e73b4ded08c138
Bavlly <bavlly2008@hotmail.com>
2018-05-16 13:51:00 +0200

    style. css aangepast en registration.php

--- Views/admin/registration.php
+++ Views/admin/registration.php
@@ -9,71 +9,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        * {
-            box-sizing: border-box;
-        }
-
-        /* Add padding to containers */
-        .container {
-            padding: 16px;
-            background-color: white;
-            background: transparent;
-            color: #fff;
-        }
-
-        /* Full-width input fields */
-        input[type=text], input[type=password] {
-            width: 100%;
-            padding: 15px;
-            margin: 5px 0 22px 0;
-            display: inline-block;
-            border: none;
-            background: #f1f1f1;
-        }
-
-        input[type=text]:focus, input[type=password]:focus {
-            background-color: #ddd;
-            outline: none;
-        }
-
-        /* Overwrite default styles of hr */
-        hr {
-            border: 1px solid #f1f1f1;
-            margin-bottom: 25px;
-        }
-
-        /* Set a style for the submit button */
-        .registerbtn {
-            background-color: #4CAF50;
-            color: white;
-            padding: 16px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-            opacity: 0.9;
-        }
-
-        .registerbtn:hover {
-            opacity: 1;
-        }
-
-        /* Add a blue text color to links */
-        a {
-            color: dodgerblue;
-        }
-
-        /* Set a grey background color and center the text of the "sign in" section */
-        .signin {
-            background-color: #f1f1f1;
-            text-align: center;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="style.css"
 </head>
 
 <form method="post" action="index.php?controller=admin/admin&action=add">
@@ -82,7 +18,7 @@
         <label for="email"><b>Voornaam</b> <input type="text" name="voornaam" required><br>
             <b>Tussenvoegsel</b> <input type="text" name="tussenvoegsel"><br>
             <b>Achternaam</b> <input type="text" name="achternaam" required><br>
-            <b>Email</b> <input type="text" name="email" required><br>
+            <b>Email</b> <input type="email" name="email" required><br>
             <b>Gebruikernaam</b> <input type="text" name="gebruikersnaam" required><br>
             <b>Wachtwoord</b> <input type="password" name="wachtwoord" required><br>
             <b>Student-/Werknemersnummer</b> <input type="text" name="nummer" ><br>
@@ -91,6 +27,6 @@
             <input type="checkbox" name="docent"> Docent<br>
             <input type="checkbox" name="SLB"> Studieloopbaan begeleider<br>
             <input type="checkbox" name="admin"> Admin<br>
-            <input type="submit" value="Verstuur">
+            <input type="submit" value="Verstuur" class="registerbtn">
     </div>
 </form>

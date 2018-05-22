5559eff9370bd68fdd90377d38cfdc8537f87db2
Bavlly <bavlly2008@hotmail.com>
2018-05-22 15:54:10 +0200

    welkom

--- Views/admin/allUsers.php
+++ Views/admin/allUsers.php
@@ -11,15 +11,45 @@
 <head>
     <title>Infinity</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style href="style.css" type="text/css" rel="stylesheet"></style>
+    <style>
+        input[type=text] {
+            width: 130px;
+            box-sizing: border-box;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            background-color: transparent;
+            background-image: url("/htdocs/resources/avatar1.jpg");
+            background-position: 10px 10px;
+            background-repeat: no-repeat;
+            padding: 12px 20px 12px 40px;
+            -webkit-transition: width 0.4s ease-in-out;
+            transition: width 0.4s ease-in-out;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+
+        }
+
+
+
+        input[type=text]:focus {
+            width: 100%;
+        }
+    </style>
 </head>
 
 
 <h1>Gebruikers</h1>
 
+<!--<form action="index.php?controller=admin/admin&action=search" method="POST">-->
+<!--    <input type= "text" name="query">-->
+<!--    <input type= "submit" value="Search">-->
+<!--</form>-->
+
 <form action="index.php?controller=admin/admin&action=search" method="POST">
-    <input type= "text" name="query">
-    <input type= "submit" value="Search">
+    <input type="text" name="query" placeholder="Search..">
 </form>
 
 <table id="customers" class="customers">

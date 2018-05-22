69a0f735a60aeeb5d81f3c169e13fbbb374bac51
Rick <s1118875@student.windesheim.nl>
2018-05-22 15:30:57 +0200

    Maken van Les

--- Views/admin/newLesson.php
+++ Views/admin/newLesson.php
@@ -5,7 +5,9 @@
  * Date: 17/05/2018
  * Time: 11:57
  */
+require_once APP_PATH . '/Model/admin.php';
 ?>
+
 
 <html>
 <head>
@@ -21,8 +23,26 @@
             <b>Datum</b> <input type="date" name="date" required><br>
             <b>Starttijd</b> <input type="time" name="startTime" required><br>
             <b>Eindtijd</b> <input type="time" name="endTime" required><br>
-            <b>Klas</b> <input type="text" name="class" required><br>
-            <b>Docent</b> <input type="text" name="teacher" required><br>
+            <select name="class" size="1">
+                <?php
+                $classes = getAllClasses();
+                foreach($classes as $class){
+                    $ID = $class['ID'];
+                    $name = $class['naam'];
+                    echo "<option value=$ID>$name</option>";
+                }
+                ?>
+            </select>
+            <select name="teacher" size="1">
+                <?php
+                $teachers = getAllTeachers();
+                foreach($teachers as $teacher){
+                    $ID = $teacher['ID'];
+                    $number = $teacher['nummer'];
+                    echo "<option value=$ID>$number</option>";
+                }
+                ?>
+            </select>
             <input type="submit" value="Verstuur" class="registerbtn">
     </div>
 </form>

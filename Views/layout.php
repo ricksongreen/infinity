ed408d9a5ed3159ee9b90bfe787b5251007c0de1
Rick <s1118875@student.windesheim.nl>
2018-05-24 10:52:47 +0200

    fixed

--- Views/layout.php
+++ Views/layout.php
@@ -61,9 +61,9 @@
     if($_SESSION["ingelogd"] == true){
         echo "<li><a href='index.php?controller=home&action=homepage'>Home</a></li>";
         if($_SESSION["rechten"] == 'admin' or $_SESSION["rechten"] == 'slbadmin'){
-            echo "<li> <a href='index.php?controller=admin/admin&action=addForm'>Registratie</a></li>";
-            echo "<li> <a href='index.php?controller=admin/admin&action=showUsers'>Student List</a></li>";
-            echo "<li> <a href='index.php?controller=admin/admin&action=addClassForm'>Klassen Aanmaken</a></li>";
+            echo "<li> <a href='index.php?controller=admin&action=addForm'>Registratie</a></li>";
+            echo "<li> <a href='index.php?controller=admin&action=showUsers'>Student List</a></li>";
+            echo "<li> <a href='index.php?controller=admin&action=addClassForm'>Klassen Aanmaken</a></li>";
         }
         echo "<li style='float:right'> <a href='index.php?controller=home&action=logout'>Log out</a></li>";
         echo "<li style=\"float:right\"><a  href='index.php?controller=home&action=homepage'>T08 Infinity</a></li>";

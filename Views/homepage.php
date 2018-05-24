87da9dd4d1b8a8d17a49c5e50a38a444e0cfc2ce
Sten <s1118010@student.windesheim.nl>
2018-05-24 14:02:25 +0200

    rooster toegevoegd

--- Views/homepage.php
+++ Views/homepage.php
@@ -14,3 +14,221 @@ if($_SESSION['rechten'] == 'docent' or $_SESSION['rechten'] == 'slb' or $_SESSIO
 }
 ?>
 
+<div class="container1">
+    <div class="row">
+        <div class="col-12">
+            <div class="row day-columns">
+                <div class="day-column">
+                    <div class="day-header">Monday</div>
+                    <div class="day-content">
+                        <div class="event gray">
+                            <span class="title">Event Name</span>
+
+                            <span>4 Assets</span><br>
+                            <span>4 Assets</span>
+                            <span>4 Assets</span>
+                            <span>4 Assets</span>
+                            <span>20:00</span>
+
+                        </div>
+                        <div class="event orange">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>3 Assets</span>
+                                <span>21:30</span>
+                            </footer>
+                        </div>
+                        <div class="event red">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+                            </footer>
+                        </div>
+                        <div class="event blue">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="day-footer">4 tasks</div>
+                </div>
+                <div class="day-column">
+                    <div class="day-header">Tuesday</div>
+                    <div class="day-content">
+                        <div class="event purple">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+
+                        <div class="event blue">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="day-footer">2 Tasks</div>
+                </div>
+                <div class="day-column">
+                    <div class="day-header">Wednesday</div>
+                    <div class="day-content">
+                        <div class="event yellow">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event navy">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event red">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event blue">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="day-footer">4 tasks</div>
+                </div>
+                <div class="day-column">
+                    <div class="day-header">Thursday</div>
+                    <div class="day-content">
+                        <div class="event purple">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event navy">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event orange">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event red">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event blue">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="day-footer">5 Tasks</div>
+                </div>
+                <div class="day-column">
+                    <div class="day-header">Friday</div>
+                    <div class="day-content">
+                        <div class="event purple">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event navy">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event yellow">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event navy">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event red">
+                <span class="title">Dit is les 3
+             </span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event blue">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                        <div class="event orange">
+                            <span class="title">Event Name</span>
+                            <footer>
+                                <span>Prop1</span>
+                                <span>20:30</span>
+
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="day-footer">7 Tasks</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
